7cfdb055464fa1d0e6b1b28658dc10fc72cae432
Amrit Jhatu <a_jhatu@hotmail.com>
2023-04-29 14:54:05 -0700

    reverting by undoing in code.

--- indextest.js
+++ indextest.js
@@ -141,15 +141,6 @@ app.get('/signup', (req,res) => {
     <button>Submit</button>
     </form>
     `;
-    if (missingUsername) {
-        html += "<br> username is required";
-    }
-    if (missingPassword) {
-        html += "<br> password is required";
-    }
-    if (missingEmail) {
-        html += "<br> email is required";
-    }
     res.send(html);
 });
 
